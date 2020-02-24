7332293994386f99ff0c73da236d80ccb6dd3f11
Tom Pocius <poci14@gmail.com>
2020-02-23 20:57:39 -0600

    centered

--- client/src/components/InfoMeeting.js
+++ client/src/components/InfoMeeting.js
@@ -2,77 +2,62 @@ import React, { useState, useEffect } from 'react';
 
 function InfoMeeting() {
   return (
-    <div class="w-full max-w-xs">
-      <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <div className="w-1/2 mx-auto">
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <div className="flex">
           <div className="w-1/2">
-            <div class="mb-4">
-              <label
-                class="block text-gray-700 text-sm font-bold mb-2"
-                for="date"
-              >
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2">
                 Date
               </label>
               <input
-                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="date"
                 type="date"
                 placeholder="Username"
               ></input>
             </div>
-            <div class="mb-4">
-              <label
-                class="block text-gray-700 text-sm font-bold mb-2"
-                for="time"
-              >
-                Time
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2">
+                Location
               </label>
               <input
-                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="time"
-                type="time"
-                placeholder="Username"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="location"
+                type="text"
+                placeholder="Location"
               ></input>
             </div>
-            <div class="mb-4">
-              <label
-                class="block text-gray-700 text-sm font-bold mb-2"
-                for="name"
-              >
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2">
                 Meeting Name
               </label>
               <input
-                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="name"
                 type="text"
                 placeholder="Meeting"
               ></input>
             </div>
           </div>
-          <div className="w-1/2">
-            <div class="mb-4">
-              <label
-                class="block text-gray-700 text-sm font-bold mb-2"
-                for="time"
-              >
-                Location
+          <div className="w-1/2 px-3">
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2">
+                Time
               </label>
               <input
-                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="location"
-                type="text"
-                placeholder="Location"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="time"
+                type="time"
+                placeholder="Username"
               ></input>
             </div>
-            <div class="mb-4">
-              <label
-                class="block text-gray-700 text-sm font-bold mb-2"
-                for="location"
-              >
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2">
                 Duration
               </label>
               <input
-                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="duration"
                 type="number"
                 placeholder="Duration"
