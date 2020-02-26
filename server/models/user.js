702d0b723a1dfba362465dce4ba73d2f6f399fbd
Andrew Chon <chonclan@gmail.com>
2020-02-25 23:51:38 -0600

    fixed userSchema

--- server/models/user.js
+++ server/models/user.js
@@ -3,8 +3,9 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   firstName: { type: String, required: true, minlength: 1 },
-  larstName: { type: String, required: false, minlength: 1 },
+  lastName: { type: String, required: false, minlength: 1 },
   email: { type: String, required: false },
+  position: { type: String },
   password: { type: String, required: false, minlength: 1 }
 });
 
