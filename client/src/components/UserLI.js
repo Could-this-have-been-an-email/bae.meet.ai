d997df17ca0ebc8f69584e40c5d9ea3d6c695281
Tom Pocius <poci14@gmail.com>
2020-02-26 20:26:42 -0600

    removed testing material

--- client/src/components/UserLI.js
+++ client/src/components/UserLI.js
@@ -3,21 +3,14 @@ import React from 'react';
 const UserLI = props => {
   let username = `${props.first} ${props.lname}`;
   return (
-    <div className="mb-4">
-      <input type="checkbox" name={username}></input>
+    <div>
+      <input
+        onChange={props.submitUsers}
+        type="checkbox"
+        name={username}
+      ></input>
       <label for="name">{username}</label>
     </div>
-    // <ul>
-    //   <li>
-    //     <strong>Name:</strong>
-    //   </li>
-    //   <li>
-    //     <strong>Salary:</strong> ${props.lname}
-    //   </li>
-    //   <li>
-    //     <strong>Age:</strong> {props.job}
-    //   </li>
-    // </ul>
   );
 };
 
