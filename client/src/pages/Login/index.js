92f7bfb05ee2ca203acac9b7eb0b987964e8e698
Andrew Chon <chonclan@gmail.com>
2020-02-29 11:09:34 -0600

    login page

---
+++ client/src/pages/Login/index.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from 'react';
+
+function Login() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log('email is ' + email);
+    console.log('password is ' + password);
+  };
+
+  console.log('email', email);
+  console.log('password', password);
+  return (
+    <div>
+      <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full">
+          <div>
+            {/* <img
+              className="mx-auto h-12 w-auto"
+              href="../../../public/assets/img/baelogo2.png"
+              alt="BAE"
+            /> */}
+            <h2 className="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
+              Login to your account
+            </h2>
+          </div>
+          <form className="mt-8" onSubmit={handleSubmit}>
+            <input type="hidden" name="remember" value="true" />
+            <div className="rounded-md shadow-sm">
+              <div>
+                <input
+                  aria-label="Email address"
+                  name="email"
+                  type="email"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                  placeholder="Email address"
+                  onChange={e => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="-mt-px">
+                <input
+                  aria-label="Password"
+                  name="password"
+                  type="password"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                  placeholder="Password"
+                  onChange={e => setPassword(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <div className="mt-6 flex items-center justify-between">
+              <div className="text-sm leading-5">
+                <a
+                  href="#"
+                  className="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+            </div>
+
+            <div className="mt-6">
+              <button
+                type="submit"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
+              >
+                <span className="absolute left-0 inset-y pl-3">
+                  <svg
+                    className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400 transition ease-in-out duration-150"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </span>
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
