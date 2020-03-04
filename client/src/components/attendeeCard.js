74c4b136d12879adf69f9bb6b6ab1564f4ca5297
TonyV <tvester1487@gmail.com>
2020-03-03 21:17:38 -0600

    Attendees Array

--- client/src/components/attendeeCard.js
+++ client/src/components/attendeeCard.js
@@ -1,6 +1,9 @@
 import React from "react";
 
-function attendeeCard() {
+function attendeeCard(props) {
+  console.log("props firstname", props.attendee.firstName);
+  // const lastName = props.attendee.lastName;
+  // const attendeeName = props.attendee;
   return (
     <div class="max-w-sm rounded overflow-hidden">
       <div class="grid grid-cols-6 gap-1 shadow border">
@@ -10,7 +13,9 @@ function attendeeCard() {
             class="rounded-full border-solid border-white border-2 mt-3 mb-3"
           />
         </div>
-        <div class="col-span-5 flex items-center justify-center">User</div>
+        <div class="col-span-5 flex items-center justify-center">
+          {props.attendee.firstName}
+        </div>
       </div>
     </div>
   );
