cd6772809aa7100401845b4d8dfafcc7b3d679fc
Andrew Chon <chonclan@gmail.com>
2020-03-04 23:06:41 -0600

    landing page WIP

--- client/src/pages/Landing/index.js
+++ client/src/pages/Landing/index.js
@@ -9,7 +9,7 @@ function Landing() {
     AOS.init();
   });
   return (
-    <div className="bg-gray-200">
+    <div className="bg-gray-100">
       <Navbar />
 
       <div>
@@ -70,7 +70,7 @@ function Landing() {
                     Spend your meetings talking about what you want to talk
                     about
                   </p>
-                  <p className="text text-gray-600">
+                  <p className="text-gray-600">
                     Upvote on items that you want to discuss. Downvote the items
                     that aren't important. Let your manger know what the
                     employees want.
@@ -81,49 +81,70 @@ function Landing() {
           </div>
         </section>
 
-        <section
-          data-aos="fade-up"
-          className="md:flex md:flex-row-reverse md:mx-24 vh"
-        >
-          <div className="flex justify-center md:w-2/4">
-            <img
-              className="self-center"
-              src="https://via.placeholder.com/500x500?text=Cool+Product+Image2"
-            />
-          </div>
-          <div className="md:w-2/4 md:py-20 flex justify-center">
-            <div className="self-center p-6">
-              <p className="text-5xl pb-6">
-                Never sit in a meeting thinking, "this could have been an
-                email," again
-              </p>
-              <p className="text-xl text-gray-600">
-                We are putting information into you BAE (Been An Email) items.
-                Hey, maybe everything will be moved in there and we can skip the
-                meeting entirely
-              </p>
+        <section data-aos="fade-up" className="md:p-20 vh">
+          <div className="md:flex md:flex-row-reverse bg-purple-300">
+            <div className="flex justify-center p-10 md:w-2/4">
+              <img
+                className="self-center"
+                src="https://via.placeholder.com/500x500?text=Cool+Product+Image2"
+              />
+            </div>
+            <div className="md:w-2/4">
+              <div className="md:w-9/12 lg:w-3/5 h-center v-offset">
+                <div className=" self-center py-32 px-4 md:px-8 lg:px-12 bg-white shadow rounded">
+                  <p className="font-medium text-purple-300">B.A.E.</p>
+                  <p className="text-2xl font-extrabold pb-3">
+                    Never sit in a meeting thinking, "this could have been an
+                    email," again
+                  </p>
+                  <p className="text-gray-600">
+                    We are putting information into your BAE (Been An Email)
+                    items. Hey, maybe everything will be moved in there and we
+                    can skip the meeting entirely
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </section>
 
-        <section data-aos="fade-up" className="md:flex  md:mx-24 vh">
-          <div className="flex justify-center md:w-2/4">
-            <img
-              className="self-center"
-              src="https://via.placeholder.com/500x500?text=Meetings+Page"
-            />
-          </div>
-          <div className="md:w-2/4 md:py-20 flex justify-center">
-            <div className=" self-center p-6">
-              <p className="text-5xl pb-6">Different</p>
-              <p className="text-xl text-gray-600">explanations</p>
-              <p className="text-xl text-gray-600">of</p>
-              <p className="text-xl text-gray-600">the</p>
-              <p className="text-xl text-gray-600">meeting</p>
-              <p className="text-xl text-gray-600">page</p>
+        <section data-aos="fade-up" className="md:p-20 vh">
+          <div className="md:flex bg-teal-300">
+            <div className="flex justify-center p-10 md:w-2/4">
+              <img
+                className="self-center"
+                src="https://via.placeholder.com/500x500?text=Cool+Product+Image1"
+              />
+            </div>
+            <div className="md:w-2/4">
+              <div className="md:w-9/12 lg:w-3/5 h-center v-offset">
+                <div className=" self-center py-32 px-4 md:px-8 lg:px-12 bg-white shadow rounded">
+                  <p className="font-medium text-teal-300">
+                    Woah Meeting Page Example
+                  </p>
+                  <p className="text-2xl font-extrabold pb-3">Different</p>
+                  <p className="text-gray-600">explanations</p>
+                  <p className="text-gray-600">of</p>
+                  <p className="text-gray-600">the</p>
+                  <p className="text-gray-600">meeting</p>
+                  <p className="text-gray-600">page</p>
+                </div>
+              </div>
             </div>
           </div>
         </section>
+        <section className="flex flex-col p-8">
+          <p className="text-center p-4 text-3xl font-bold">
+            Check us out now!
+          </p>
+          <a
+            href="/signup"
+            className="button is-medium is-rounded is-danger self-center"
+          >
+            Get started!
+          </a>
+        </section>
+        <footer></footer>
       </div>
     </div>
   );
