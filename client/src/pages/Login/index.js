eaeb483fd46c76b7d8dc89462ccbdb82b7f12a43
Andrew Chon <chonclan@gmail.com>
2020-03-05 18:35:21 -0600

    restyled Login page WIP

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,49 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import API from '../../utils/API';
 
-
 function Login() {
-  // const [email, setEmail] = useState();
-  // const [password, setPassword] = useState();
-
   const handleSubmit = e => {
     e.preventDefault();
     let checkedUser = {
       email: e.target.email.value,
       password: e.target.password.value
-    }
+    };
 
     API.checkUser(checkedUser)
-      .then(function (result) {
-        console.log('results', result)
+      .then(result => {
+        console.log('results', result);
         window.location.replace(`/user/${result.data._id}`);
       })
-    // .catch(function (err) {
-    //   console.log(err);
-    // });
-
-
-
-    // console.log('email is ' + email);
-    // console.log('password is ' + password);
+      .catch(err => {
+        console.log(err);
+        alert('wrong login, will style later');
+      });
   };
 
   API.getUserAuthen((req, res) => {
-    console.log('touchdown res res', res)
-  })
-
-  // console.log('email', email);
-  // console.log('password', password);
+    console.log('touchdown res res', res);
+  });
   return (
     <div>
       <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full">
           <div>
-            {/* <img
-              className="mx-auto h-12 w-auto"
-              href="../../../public/assets/img/baelogo2.png"
-              alt="BAE"
-            /> */}
             <h2 className="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
               Login to your account
             </h2>
@@ -57,9 +41,9 @@ function Login() {
                   name="email"
                   type="email"
                   required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                  className="input mb-3"
                   placeholder="Email address"
-                // onChange={e => setEmail(e.target.value)}
+                  // onChange={e => setEmail(e.target.value)}
                 />
               </div>
               <div className="-mt-px">
@@ -68,42 +52,19 @@ function Login() {
                   name="password"
                   type="password"
                   required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                  className="input"
                   placeholder="Password"
-                // onChange={e => setPassword(e.target.value)}
+                  // onChange={e => setPassword(e.target.value)}
                 />
-              </div>
-            </div>
-
-            <div className="mt-6 flex items-center justify-between">
-              <div className="text-sm leading-5">
-                <a
-                  href="#"
-                  className="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150"
-                >
-                  Forgot your password?
-                </a>
               </div>
             </div>
 
             <div className="mt-6">
               <button
                 type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
+                className="button is-fullwidth is-warning"
+                // className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
               >
-                <span className="absolute left-0 inset-y pl-3">
-                  <svg
-                    className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400 transition ease-in-out duration-150"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </span>
                 Login
               </button>
             </div>
