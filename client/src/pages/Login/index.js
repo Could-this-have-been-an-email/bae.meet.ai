80436f54df9510d6b152c60bd81758374e92aeda
Andrew Chon <chonclan@gmail.com>
2020-03-05 19:21:47 -0600

    log in

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import API from '../../utils/API';
 
 function Login() {
@@ -43,7 +43,6 @@ function Login() {
                   required
                   className="input mb-3"
                   placeholder="Email address"
-                  // onChange={e => setEmail(e.target.value)}
                 />
               </div>
               <div className="-mt-px">
@@ -54,17 +53,12 @@ function Login() {
                   required
                   className="input"
                   placeholder="Password"
-                  // onChange={e => setPassword(e.target.value)}
                 />
               </div>
             </div>
 
             <div className="mt-6">
-              <button
-                type="submit"
-                className="button is-fullwidth is-warning"
-                // className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
-              >
+              <button type="submit" className="button is-fullwidth is-warning">
                 Login
               </button>
             </div>
