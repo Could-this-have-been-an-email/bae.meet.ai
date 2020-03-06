bf45d9803e12f0969472d6c3df1d4c594595e703
Tom Pocius <poci14@gmail.com>
2020-03-05 20:23:15 -0600

    new promises for meeting array of users working

--- client/src/components/attendeeCard.js
+++ client/src/components/attendeeCard.js
@@ -1,19 +1,19 @@
 import React from "react";
 
 function attendeeCard(props) {
-  console.log("props firstname", props.attendee.firstName);
+  console.log("props firstname", props.attendee);
   // const lastName = props.attendee.lastName;
   // const attendeeName = props.attendee;
   return (
-    <div class="max-w-sm rounded overflow-hidden">
-      <div class="grid grid-cols-6 gap-1 shadow border">
-        <div class="col-span-1">
+    <div className="max-w-sm rounded overflow-hidden">
+      <div className="grid grid-cols-6 gap-1 shadow border">
+        <div className="col-span-1">
           <img
             src="https://i.imgur.com/8Km9tLL.jpg"
-            class="rounded-full border-solid border-white border-2 mt-3 mb-3"
+            className="rounded-full border-solid border-white border-2 mt-3 mb-3"
           />
         </div>
-        <div class="col-span-5 flex items-center justify-center">
+        <div className="col-span-5 flex items-center justify-center">
           {props.attendee.firstName}
         </div>
       </div>
