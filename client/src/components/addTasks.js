a34626ba64c04ce972ff683b74a3edc0a186e79c
TonyV <tvester1487@gmail.com>
2020-03-07 09:22:17 -0600

    Task styling

--- client/src/components/addTasks.js
+++ client/src/components/addTasks.js
@@ -2,18 +2,19 @@ import React from "react";
 
 function AddTasks(props) {
   return (
-    <div className="grid grid-cols-3 ml-5 container flex items-center">
-      <div className="col-span-2 flex items-center py-1 px-1 -mr-5">
+    <div className="pl-10 flex container flex items-center">
+      <div className="w-5/6 py-1 px-1">
         <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          className=" appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           id="task"
           type="text"
           placeholder="Add a Task"
         ></input>
-
+      </div>
+      <div className="flex w-1/6 justify-end">
         <button
-          onClick={() => props.handleTask(props.agenda._id)}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline .w-auto"
+          onClick={() => props.handleTask(props._id)}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline"
           type="button"
         >
           Add Task
--- client/src/components/agenda.js
+++ client/src/components/agenda.js
@@ -5,8 +5,8 @@ function Agenda(props) {
   var tasks = props.tasks;
 
   return (
-    <div className="test">
-      <div className="grid grid-cols-3 gap-1 px-1 border shadow container flex items-center">
+    <div>
+      <div className="grid grid-cols-3 gap-1 p-1 border shadow container flex items-center">
         <div className="col-span-1 flex items-center">
           <input type="checkbox" />
         </div>
@@ -20,24 +20,35 @@ function Agenda(props) {
           className="col-start-3 flex items-center justify-end"
         >
           <button
-            className="fa fa-arrow-up"
+            className="fa fa-arrow-up px-1"
             onClick={() => props.handleUpVote(props.agenda._id)}
           ></button>
           <button
-            className="fa fa-arrow-down"
+            className="fa fa-arrow-down px-1"
             onClick={() => props.handleDownVote(props.agenda._id)}
           ></button>
 
-          <span>{props.agenda.vote}</span>
+          <span className="px-1">{props.agenda.vote}</span>
         </div>
       </div>
-      <div className="grid col-start-3">
+      <div className="grid col-start-3 pl-10">
         {props.tasks ? (
           <>
             {tasks.map(task => {
               return (
-                <div className="ml-5 border" key={task._id}>
-                  {task.task}
+                <div className="pt-1">
+                  <div
+                    className="grid grid-cols-3 p-1 border w-full"
+                    key={task._id}
+                  >
+                    <div className="col-span-1 flex items-center">
+                      <input type="checkbox" />
+                    </div>
+                    <div className="grid "> {task.task}</div>
+                    <div className="flex items-center justify-end">
+                      <i class="fa fa-ellipsis-v"></i>
+                    </div>
+                  </div>
                 </div>
               );
             })}
