48387d866378fb1a492057e73ebfd036a04fd341
Andrew Chon <chonclan@gmail.com>
2020-03-07 12:13:28 -0600

    meeting card

--- client/src/components/MeetingCard/index.js
+++ client/src/components/MeetingCard/index.js
@@ -2,26 +2,30 @@ import React from 'react';
 
 function MeetingCard(props) {
   return (
-    <div className="box">
-      <article className="media">
-        <div className="media-content">
-          <div className="content">
-            <p className="is-marginless">
-              <strong>{props.name} </strong>
-              <small>
-                On {props.date} at {props.time} for {props.duration} minutes
-              </small>
-            </p>
-            <p>Goal: {props.outcome}</p>
+    <>
+      <div className="border-gray-300 border-solid border rounded p-3 m-4">
+        <article className="media">
+          <div className="media-content">
+            <div className="content">
+              <p className="is-marginless">
+                <strong>{props.name} </strong>
+                <small>
+                  On {props.date} at {props.time} for {props.duration} minutes
+                </small>
+              </p>
+              <p>Goal: {props.outcome}</p>
+            </div>
           </div>
-        </div>
-        <div className="media-right">
-          <a className="button" href={`/meeting/${props.meetingID}`}>
-            Go to meeting
-          </a>
-        </div>
-      </article>
-    </div>
+          <div className="media-right">
+            <a className="button" href={`/meeting/${props.meetingID}`}>
+              Go to meeting
+            </a>
+          </div>
+        </article>
+      </div>
+
+      <hr className="mx-10"></hr>
+    </>
   );
 }
 
