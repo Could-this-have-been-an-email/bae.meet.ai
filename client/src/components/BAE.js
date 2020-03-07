876fca82f5f5cbff46ead9038e81064289084982
Tom Pocius <poci14@gmail.com>
2020-03-07 13:27:33 -0600

    strated styling tasks with position

--- client/src/components/BAE.js
+++ client/src/components/BAE.js
@@ -28,24 +28,26 @@ const BAE = (props) => {
           <span className="px-1">{props.agenda.vote}</span>
         </div>
       </div>
-      {props.tasks ? (
-        <>
-          {props.tasks.map(task => {
-            return (
-              <div className="pt-1">
-                <div
-                  className="grid grid-cols-3 p-1 border w-full"
-                  key={task._id}
-                >
-                  <div className="grid "> {task.task}</div>
+      <div className="mx-16 pt-2">
+        {props.tasks ? (
+          <>
+            {props.tasks.map(task => {
+              return (
+                <div className="py-1">
+                  <div
+                    className="p-1 bg-red-200 rounded-lg flex justify-center border"
+                    key={task._id}
+                  >
+                    <div className="m-2"> {task.task}</div>
+                  </div>
                 </div>
-              </div>
-            );
-          })}
-        </>
-      ) : (
-          <></>
-        )}
+              );
+            })}
+          </>
+        ) : (
+            <></>
+          )}
+      </div>
     </div>
     // <div className="bg-red-500">
     //   this is the agenda: {props.agenda}
