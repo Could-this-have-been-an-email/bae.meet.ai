90a02efda785254156859e2b35fbbe13dac258ec
Katie McBride <mcbride.katieb@gmail.com>
2020-03-07 23:10:29 -0600

    working on task assign

--- client/src/components/Dropdown.js
+++ client/src/components/Dropdown.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import Agenda from "./agenda"
 
 
 function Dropdown(props) {
 
+    const [task, setTask] = useState("");
+    
+
+    function assignTask(){
+        console.log("handle task")
+        {props.attendees && props.attendees.map(attendee => {
+            console.log({attendee})
+            let id = attendee._id;
+            // console.log(props.meeting)
+            
+            // API.updateMeeting(meeting._id, {
+            //     $set: {
+            //       "userId": { id }
+            //     }
+            //   });
+
+            // task.userId.update
+            })}
+      
+      
+    };
+
+
 return(
 <div class="dropdown-content">
                             
     {props.attendees && props.attendees.map(attendee => {
-    console.log({attendee})
+    // console.log({attendee})
     return(
-    <a className="dropdown-item">{attendee.firstName}</a>
+    <a className="dropdown-item" onClick={assignTask()}>{attendee.firstName}</a>
         )
     })}
                             
---
+++ client/src/components/MeetingNotes.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Editor } from "@tinymce/tinymce-react";
+
+class MeetingNotes extends React.Component {
+  handleEditorChange = (content, editor) => {
+    console.log("Content was updated:", content);
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <Editor
+          apiKey="avgvd7u4i68a9mq24lbgo9zusv5tq1vyu4pw9xrjkt9depds"
+          initialValue="<p>This is the initial content of the editor</p>"
+          init={{
+            height: 500,
+            menubar: false,
+            plugins: [
+              "advlist autolink lists link image charmap print preview anchor",
+              "searchreplace visualblocks code fullscreen",
+              "insertdatetime media table paste code help wordcount"
+            ],
+            toolbar:
+              "undo redo | formatselect | bold italic backcolor | \
+            alignleft aligncenter alignright alignjustify | \
+            bullist numlist outdent indent | removeformat | help"
+          }}
+          onEditorChange={this.handleEditorChange}
+        />
+      </div>
+    );
+  }
+}
+
+export default MeetingNotes;
