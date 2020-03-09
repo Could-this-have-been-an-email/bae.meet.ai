6877c5c7821e380b1039c53fdd454a002323e254
Tom Pocius <poci14@gmail.com>
2020-03-08 22:31:09 -0500

    deleted function in dropdown, added key value pairs to pass back up

--- client/src/components/Dropdown.js
+++ client/src/components/Dropdown.js
@@ -1,44 +1,22 @@
 import React, { useState, useEffect } from "react";
-import Agenda from "./agenda"
 
 
 function Dropdown(props) {
 
-    const [task, setTask] = useState("");
-    
 
-    function assignTask(){
-        console.log("handle task")
-        {props.attendees && props.attendees.map(attendee => {
-            console.log({attendee})
-            let id = attendee._id;
-            // console.log(props.meeting)
-            
-            // API.updateMeeting(meeting._id, {
-            //     $set: {
-            //       "userId": { id }
-            //     }
-            //   });
 
-            // task.userId.update
+    return (
+        <div class="dropdown-content">
+
+            {props.attendees && props.attendees.map(attendee => {
+                // console.log({attendee})
+                return (
+                    <div className="dropdown-item" key={attendee._id} useridvalue={attendee._id} taskidforuser={props.taskidforuser} agendaidforuser={props.agendaidforuser} onClick={props.addUserTask}>{attendee.firstName}</div>
+                )
             })}
-      
-      
-    };
 
-
-return(
-<div class="dropdown-content">
-                            
-    {props.attendees && props.attendees.map(attendee => {
-    // console.log({attendee})
-    return(
-    <a className="dropdown-item" onClick={assignTask()}>{attendee.firstName}</a>
-        )
-    })}
-                            
-</div>
-)
+        </div>
+    )
 
 
 };
