3f30c6efa03cc68c8a2cc529172e32f0b42a97c1
Tom Pocius <poci14@gmail.com>
2020-03-09 20:38:00 -0500

    fixed issues with camel case and jsx

--- client/src/components/Dropdown.js
+++ client/src/components/Dropdown.js
@@ -5,12 +5,13 @@ function Dropdown(props) {
 
 
     return (
-        <div class="dropdown-content">
+        <div className="dropdown-content">
 
             {props.attendees && props.attendees.map(attendee => {
                 // console.log({attendee})
                 return (
-                    <div className="dropdown-item hover:bg-orange-300" key={attendee._id} useridvalue={attendee._id} taskidforuser={props.taskidforuser} agendaidforuser={props.agendaidforuser} onClick={props.addUserTask} attendeeFirstName={attendee.firstName} attendeeLastName={attendee.lastName}>{attendee.firstName} {attendee.lastName}</div>
+                    <div className="dropdown-item hover:bg-orange-300" key={attendee._id} useridvalue={attendee._id} taskidforuser={props.taskidforuser} agendaidforuser={props.agendaidforuser} onClick={props.addUserTask} attendeefirstname={attendee.firstName} attendeelastname={attendee.lastName}>{attendee.firstName} {attendee.lastName}
+                    </div>
                 )
             })}
 
