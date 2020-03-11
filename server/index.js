57f2ad2cf2a40c3904d627bad9b723714e2e9aa5
Tom Pocius <poci14@gmail.com>
2020-03-11 15:20:22 -0500

    replaced with path

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const path = require("path");
 
 
 app.use(express.static("public"));
-express.static(path_join(__dirname, '../client/build'))
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 
 app.use(session({ secret: "cats" }));
